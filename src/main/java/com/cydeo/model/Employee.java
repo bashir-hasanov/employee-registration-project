ec443b1d3276d33e5180cd19deb63b7dfc767db3
bashir-hasanov <beshir.hasanov@gmail.com>
2022-09-29 21:53:42 -0400

    Created model data class

---
+++ src/main/java/com/cydeo/model/Employee.java
@@ -0,0 +1,30 @@
+package com.cydeo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+
+
+
+
+}
