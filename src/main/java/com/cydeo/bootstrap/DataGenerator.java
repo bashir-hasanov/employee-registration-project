bf8bcfa3e5d1afceca999979909db5b514a93b81
bashir-hasanov <beshir.hasanov@gmail.com>
2022-09-29 23:25:06 -0400

    Form thymeleaf steps

---
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -0,0 +1,16 @@
+package com.cydeo.bootstrap;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataGenerator {
+
+    public static List<String> getAllStates() {
+        return Arrays.asList("Alaska", "Alabama", "Arkansas", "Arizona", "California", "Colorado", "Connecticut", "Delaware", "Florida",
+                "Georgia", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana",
+                "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska",
+                "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania","Rhode Island", "South Carolina",
+                "South Dakota", "Tennessee", "Texas", "Utah", "Virginia","Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming");
+
+    }
+}
--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -1,15 +1,13 @@
 package com.cydeo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class Employee {
 
